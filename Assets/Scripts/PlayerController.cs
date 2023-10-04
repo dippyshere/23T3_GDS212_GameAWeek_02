43b14c2626f062ea40025e7e191d9dcf99b03bad
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-10-04 16:02:55 +1100

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,10 +18,15 @@ public class PlayerController : MonoBehaviour
     private Vector3 targetPosition;
     private Quaternion targetRotation;
     private Vector3 currentVelocity = Vector3.zero;
-    public float markerOpacity = 0f;
+    private float currentSpeed = 0f;
+    private float previousSpeed = 0f;
+    private float currentAcceleration = 0f;
+    private float lerpedAcceleration = 0f;
+    private float markerOpacity = 0f;
 
     private void Update()
     {
+        // movement input
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -38,15 +43,26 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        // movement
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, acceleration, maxSpeed);
         boatReferenceRotation.transform.rotation = Quaternion.Lerp(boatReferenceRotation.transform.rotation, targetRotation, Time.deltaTime * 2f);
         
+        // marker location / opacity
         markerObject.transform.position = Vector3.Lerp(markerObject.transform.position, targetPosition, Time.deltaTime * 6f);
-        markerOpacity = Mathf.Clamp01(Mathf.Lerp(markerOpacity, (1f - Mathf.Clamp(Vector3.Distance(transform.position, targetPosition), 5f, 10f) / 5f) * -1, Time.deltaTime * 6f));
+        markerOpacity = Mathf.Clamp01(Mathf.Lerp(markerOpacity, (1f - Mathf.Clamp(Vector3.Distance(transform.position, targetPosition), 4f, 7.5f) / 4f) * -1, Time.deltaTime * 6f));
         foreach (MeshRenderer meshRenderer in markerObject.GetComponentsInChildren<MeshRenderer>())
         {
             meshRenderer.material.color = new Color(meshRenderer.material.color.r, meshRenderer.material.color.g, meshRenderer.material.color.b, markerOpacity);
         }
+        
+        // boat angle with speed
+        currentSpeed = currentVelocity.magnitude;
+        // compute rate of change of speed
+        currentAcceleration = (currentSpeed - previousSpeed) / Time.deltaTime;
+        previousSpeed = currentSpeed;
+        // lerp the acceleration to smooth it out
+        lerpedAcceleration = Mathf.Lerp(lerpedAcceleration, currentAcceleration, Time.deltaTime * 6f);
+        //boatReferenceRotation.transform.localRotation = Quaternion.Euler(lerpedAcceleration * -1f, boatReferenceRotation.transform.localRotation.y, boatReferenceRotation.transform.localRotation.z);
     }
 
     private void FixedUpdate()
