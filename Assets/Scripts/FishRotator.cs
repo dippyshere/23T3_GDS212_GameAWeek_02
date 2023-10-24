84b6685a053f2a3f3139092e6f0c1f518a763b71
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-10-25 01:34:33 +1100

    optimise performance, fix flag in builds

--- Assets/Scripts/FishRotator.cs
+++ Assets/Scripts/FishRotator.cs
@@ -8,16 +8,22 @@ public class FishRotator : MonoBehaviour
     private float speed;
     private Vector3 centrePoint;
     private float circleRadius;
+    private Renderer render;
 
     private void Start()
     {
         circleRadius = Random.Range(0.86f, 4.13f);
         speed = Random.Range(0.6f, 1.4f);
         centrePoint = transform.position;
+        render = GetComponentInChildren<Renderer>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        if (render != null && !render.isVisible)
+        {
+            return;
+        }
         angle -= speed * Time.deltaTime;
         transform.position = new Vector3(centrePoint.x + Mathf.Cos(angle) * circleRadius, centrePoint.y, centrePoint.z + Mathf.Sin(angle) * circleRadius);
         transform.LookAt(centrePoint);
